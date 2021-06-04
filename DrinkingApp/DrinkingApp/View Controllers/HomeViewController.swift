099e9f5e7bf81c4bcdbc9fc13728feb4f96dca82
jrmat <82636683+jrmat@users.noreply.github.com>
2021-06-04 17:42:37 +1200

    Add files via upload

---
+++ DrinkingApp/DrinkingApp/View Controllers/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  DrinkingApp
+//
+//  Created by Mohammed El Saka on 5/10/21.
+//  Copyright © 2021 Mohammed El Saka. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    
+   /** // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        }
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    } **/
+    
+    
+
+}
---
+++ DrinkingApp/DrinkingAppTests/DrinkingAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  DrinkingAppTests.swift
+//  DrinkingAppTests
+//
+//  Created by Mohammed El Saka on 5/10/21.
+//  Copyright © 2021 Mohammed El Saka. All rights reserved.
+//
+
+import XCTest
+@testable import DrinkingApp
+
+class DrinkingAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
