1517b636a42a6e032f2a42fd8f443ebaa1fada11
jrmat <82636683+jrmat@users.noreply.github.com>
2021-07-28 17:26:37 +1200

    Add files via upload

---
+++ Na zdrowie/Na_zdrowieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Na_zdrowieApp.swift
+//  Na zdrowie
+//
+//  Created by Jada on 27/05/21.
+//
+
+import SwiftUI
+/// Main method to run app.
+@main
+struct Na_zdrowieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
