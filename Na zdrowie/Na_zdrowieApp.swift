39b1b0689c43f6068a3459dd4fc618a88a690b30
Jake <jakemperk@gmail.com>
2021-07-28 12:24:49 +1200

    unzipped files

---
+++ Na zdrowie/Na_zdrowieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Na_zdrowieApp.swift
+//  Na zdrowie
+//
+//  Created by Jada on 27/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Na_zdrowieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
