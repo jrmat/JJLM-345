2a23104a0c6a2cb40cab446620485aa13e891dd2
Sawiiick <sawja654@student.otago.ac.nz>
2021-08-17 13:15:38 +1200

    Commit

--- NaZdrowie/NaZdrowie/NaZdrowieApp.swift
+++ NaZdrowie/NaZdrowie/NaZdrowieApp.swift
@@ -15,3 +15,15 @@ struct NaZdrowieApp: App {
         }
     }
 }
+
+var timerCount = 0
+var playerZloties = 0
+
+let timer = Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { timer in
+    timerCount += 1
+    playerZloties += 10
+    
+    if timerCount == 100 {
+        timer.invalidate()
+    }
+}
